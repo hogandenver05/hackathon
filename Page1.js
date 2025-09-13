7117fb40a8d9898f3fe927d26253bb3d4c56c65f
Will-Pond <106034736+Will-Pond@users.noreply.github.com>
2025-09-12 21:33:17 -0400

    update

--- Page1.js
+++ Page1.js
@@ -4,7 +4,11 @@ const heading = document.getElementById('myHeading');
 
 // Array of messages to cycle through
 const messages = [
-    'This is the first message.',
+    'Age 20
+    A new company is selling shares for 10 dollars each. Would you like to buy
+Buy 10 shares at 10 dollars each
+Skip this stock
+Lesson: Investing early gives your money more time to grow. Buying is a risk but could pay off later.',
     'Now you are seeing the second message!',
     'The third message is here.',
     'And this is the fourth message in the list.',
